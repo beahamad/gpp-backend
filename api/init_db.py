458b44ad325f16eb127235080b69a7fe7e6c1691
Bea <25506378+beahamad@users.noreply.github.com>
2023-08-23 13:48:20 -0300

    Update init_db.py

--- api/init_db.py
+++ api/init_db.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import database_exists, create_database
 from models import User, Phone
-from app import app, db
+from api import app, db
 from config import get_uri
 
 with app.app_context():
